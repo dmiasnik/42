2cf089dd59c6d11d4be8d3f4b9c1e61af74bc6fb
Dmitrii <141118691+dmiasnik@users.noreply.github.com>
2023-10-12 18:23:02 +0200

    Add files via upload

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: dmiasnik <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/09 16:52:56 by dmiasnik          #+#    #+#             */
-/*   Updated: 2023/10/12 12:49:37 by dmiasnik         ###   ########.fr       */
+/*   Updated: 2023/10/12 18:07:17 by dmiasnik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,39 +20,14 @@ char	*ft_strtrim(char const *s1, char const *set)
 	if (!s1)
 		return (NULL);
 	if (!set)
-		return (ft_substr(s1, 0, ft_strlen(s1) - 1));
+		return (ft_substr(s1, 0, ft_strlen(s1)));
 	start = 0;
 	while (s1[start] && ft_strchr(set, s1[start]))
 		start++;
 	end = ft_strlen(s1) - 1;
 	while (s1[end] && ft_strchr(set, s1[end]))
 		end--;
+	if (end < start)
+		return (ft_substr(s1, start, 0));
 	return (ft_substr(s1, start, end - start + 1));
 }
-/*
-void	ft_print_result(char const *s)
-{
-	int		len;
-
-	len = 0;
-	while (s[len])
-		len++;
-	write(1, s, len);
-}
-
-int		main(int argc, const char *argv[])
-{
-	char	*strtrim;
-	int		arg;
-
-{
-		char s1[] = "         1 ";
-		if (!(strtrim = ft_strtrim(s1, " ")))
-			ft_print_result("NULL");
-		else
-			ft_print_result(strtrim);
-		if (strtrim == s1)
-			ft_print_result("\nA new string was not returned");
-	}
-	return (0);
-}*/
