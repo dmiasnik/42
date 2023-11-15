e79659ba81daa016b960d36529312dffae88e97d
Dmitrii <141118691+dmiasnik@users.noreply.github.com>
2023-11-15 15:16:58 +0100

    Add files via upload

---
+++ so_long/libft/ft_lstnew.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dmiasnik <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/11 11:36:24 by dmiasnik          #+#    #+#             */
+/*   Updated: 2023/10/12 15:40:32 by dmiasnik         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*list;
+
+	list = malloc(sizeof(t_list));
+	if (list == NULL)
+		return (NULL);
+	list->content = content;
+	list->next = NULL;
+	return (list);
+}
---
+++ so_long/libft/ft_strtrim.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dmiasnik <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/09 16:52:56 by dmiasnik          #+#    #+#             */
+/*   Updated: 2023/10/12 18:07:17 by dmiasnik         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	size_t	start;
+	size_t	end;
+
+	if (!s1)
+		return (NULL);
+	if (!set)
+		return (ft_substr(s1, 0, ft_strlen(s1)));
+	start = 0;
+	while (s1[start] && ft_strchr(set, s1[start]))
+		start++;
+	end = ft_strlen(s1) - 1;
+	while (s1[end] && ft_strchr(set, s1[end]))
+		end--;
+	if (end < start)
+		return (ft_substr(s1, start, 0));
+	return (ft_substr(s1, start, end - start + 1));
+}
